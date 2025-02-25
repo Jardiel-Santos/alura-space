7c57a8f1a896ac10f026e9c3875334a19cc5ca1f
Jardiel-Santos <137847680+Jardiel-Santos@users.noreply.github.com>
2025-02-25 09:16:46 -0300

    Update settings.py

--- setup/settings.py
+++ setup/settings.py
@@ -125,6 +125,8 @@ AWS_SECRET_ACCESS_KEY = SECRET_KEY = str(os.getenv('AWS_SECRET_ACCESS_KEY'))
 
 AWS_STORAGE_BUCKET_NAME = SECRET_KEY = str(os.getenv('AWS_STORAGE_BUCKET_NAME'))
 
+AWS_S3_REGION_NAME = 'sa-east-1'
+
 AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
 
 AWS_DEFAULT_ACL = 'public-read'
@@ -145,9 +147,9 @@ AWS_HEADERS = {
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-DEFAULT_FILE_STORAGE = "storages.backends.s3.S3Storage"
+DEFAULT_FILE_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
 
-STATICFILES_STORAGE = "storages.backends.s3.S3Storage"
+STATICFILES_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
 
 STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/static/'
 
